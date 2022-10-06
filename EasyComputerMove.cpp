daa4c87b0eb752303dcbe10f9ed4fd76e72a439c
isaacGrinboim <102624969+isaacGrinboim@users.noreply.github.com>
2022-10-06 10:50:28 +0300

    Add files via upload

--- EasyComputerMove.cpp
+++ EasyComputerMove.cpp
@@ -7,18 +7,21 @@ void EasyComputerMove::makeMove() {
      //צריך לשמור זמנית את האברים הריקים בתור צמדים
      //צריך להגריל מי מהם יהיה זה שבוחרים
      //צריך לחכות שנייה ורק אז להדפיס
-    /* int randomNumber = rand()%(BOARD_SIZE*BOARD_SIZE);
-     for (int i = 0; i < BOARD_SIZE*BOARD_SIZE; i++)
-     {
-          if(i==randomNumber){
-               if(board->isFree(i/BOARD_SIZE,i%BOARD_SIZE)){
-                    board->putSymbol(i/BOARD_SIZE ,i%BOARD_SIZE, symbol);
-               }
+     int randomNumber = rand()%(BOARD_SIZE*BOARD_SIZE);
+
+     for(int i = 0; i<BOARD_SIZE*BOARD_SIZE; ++i){
+          cout<<endl<<"random: "<<randomNumber<<endl;
+          cout<<" i/3 = "<<randomNumber/3<<" and i%3 = "<<randomNumber%3<<endl;
+          cout<<"isFree: "<<board->isFree(randomNumber/BOARD_SIZE,randomNumber%BOARD_SIZE)<<endl<<endl;
+
+          if(board->isFree(randomNumber/BOARD_SIZE,randomNumber%BOARD_SIZE)){
+               sleep_for(nanoseconds(10));
+               sleep_until(system_clock::now() + seconds(1));
+               board->putSymbol((randomNumber/BOARD_SIZE+1),(randomNumber%BOARD_SIZE+1), symbol);
+               return;
           }
-          randomNumber=(randomNumber+1)%(BOARD_SIZE*BOARD_SIZE);
-          i=0;
-     }*/
-     sleep_for(nanoseconds(10));
-     sleep_until(system_clock::now() + seconds(1));
-     board->putSymbol(2,2, symbol);
+          else{
+               randomNumber = (randomNumber+1)%(BOARD_SIZE*BOARD_SIZE);
+          }
+     }
 }
