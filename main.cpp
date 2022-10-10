be0d8610d309d1d9cebf33afa7f9e65e97d4e23e
isaacGrinboim <102624969+isaacGrinboim@users.noreply.github.com>
2022-10-11 00:41:25 +0300

    Now can Play Multiple games in a row

--- main.cpp
+++ main.cpp
@@ -4,26 +4,41 @@ int main(){
      //CurrentTurn playerStarts = CurrentTurn::Player;
      //turn = Player wants to be first -> turn = 0;
 
-     Score score;
+      Score score;
+      bool keepPlay = true;
+      //string playing;
+
+      while (keepPlay)
+      {
+         Game newGame(CurrentTurn::Player);
+         GameState state;
 
 
+          while(!newGame.gameOver(&state)){
+              newGame.makeMove();            
+          }
+          score+=state;
+            cout<<"Game State is: "<<state<<endl;
+            cout<<"Game score is: "<<score<<endl;
+            cout<<"Do you wish to play another game?"<<endl;
+      }
       
 
-     Game newGame(CurrentTurn::Player);
-     GameState state;
-     while(!newGame.gameOver(&state)){
-        newGame.makeMove();
+   //   Game newGame(CurrentTurn::Player);
+   //   GameState state;
+   //   while(!newGame.gameOver(&state)){
+   //      newGame.makeMove();
      //                        //יהיה אפשר לקבל את המידע על סוף המשחק
      // אולי סתם בשביל האתגר נעשה מחלקה של מידע על משחק, עם 3 משתנים ויהיה אופרטור פלוס שווה
      // שמקבל מימין גיים סטייט ולפי כל אחד נוסיף בהתאם
-     }//יהיו 3 משתנים שמחזיקים כמה נצחונות כמה הפסדים וכמה תיקו היו
+     //}//יהיו 3 משתנים שמחזיקים כמה נצחונות כמה הפסדים וכמה תיקו היו
      // for (int i = 0; i < 20; i++){
      //      newGame.makeMove();
-     // }
-     score+=state;
+   //   // }
+   //   score+=state;
 
-     cout<<"Game State is: "<<state<<endl;
-     cout<<"Game score is: "<<score;
+   //   cout<<"Game State is: "<<state<<endl;
+   //   cout<<"Game score is: "<<score;
      int g;
      cin>>g;
      return 0;
